dc0d09eaa0a45bb4bb6513f21eb1ce548289bdc8
lljl500220 <1723377108@qq.com>
2023-09-21 22:10:50 +0800

    title

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         child: MaterialApp(
           title: 'Flutter Demo',
           theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.purpleAccent),
             useMaterial3: true,
           ),
           home: const MyHomePage(title: 'Flutter Demo Home Page223'),
@@ -45,36 +45,72 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var textFieldSearch = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyHomeState>();
     var word = appState.word;
     return Scaffold(
-        appBar: AppBar(
-            backgroundColor: Theme.of(context).colorScheme.primary,
-            title: Text(
-              widget.title,
-              style: const TextStyle(color: Colors.white),
-            )),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+      appBar: AppBar(
+          title: Row(
             children: [
-              WordText(word: word),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                style:
-                    ElevatedButton.styleFrom(minimumSize: const Size(200, 50)),
-                onPressed: () {
-                  appState.add();
-                },
-                child: Text(
-                  word.toString(),
-                ),
+              const CircleAvatar(
+                backgroundImage: AssetImage('images/avatar1.jpg'),
               ),
+              const SizedBox(
+                width: 20,
+              ),
+              Expanded(
+                  child: TextField(
+                controller: textFieldSearch,
+                cursorColor: Colors.black,
+                style: const TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                    suffixIcon: const Icon(Icons.search),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50),borderSide: BorderSide(color: Colors.grey)),
+                    hintText: '输入内容',
+                    fillColor: const Color.fromRGBO(255, 255, 255, 0.5),
+                    filled: true,
+                    contentPadding: const EdgeInsets.all(10)),
+              ))
             ],
-          ),
-        ));
+          )),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            WordText(word: word),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(minimumSize: const Size(200, 50)),
+              onPressed: () {
+                appState.add();
+              },
+              child: Text(
+                word.toString(),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar:
+          BottomNavigationBar(items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: "首页",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: "搜索",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: "我的",
+        ),
+      ]),
+    );
   }
 }
 
@@ -98,7 +134,7 @@ class WordText extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(20),
         child: Text(
-          'time count is ${word.toString()}',
+          'tap count : ${word.toString()}',
           style: style,
         ),
       ),
